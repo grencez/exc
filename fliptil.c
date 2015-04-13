0e1d38418fb9e977ef9de9e7e9a1c34837a2915b
Alex Klinkhamer <grencez@codelove.org>
2015-04-13 05:24:42 -0400

    Add pazucarlo and fliptil programs

---
+++ fliptil.c
@@ -0,0 +1,45 @@
+
+#include "cx/syscx.h"
+#include "cx/ofile.h"
+
+int main(int argc, char** argv)
+{
+  int argi =
+    (init_sysCx (&argc, &argv),
+     1);
+  int excess = 0;
+  ujint ntrials = 0;
+
+  if (argi +1 != argc)  return 1;
+  excess = atoi(argv[argi]);
+
+  {
+    int x = 0;
+    byte b = 0;
+    uint i = NBitsInByte;
+    while (x < excess && x > -excess) {
+      if (i == NBitsInByte) {
+        i = 0;
+        randomize(&b, sizeof(b));
+      }
+      x += (b % 2 == 1) ? 1 : 0;
+      b /= 2;
+      ++ i;
+      x += (b % 2 == 1) ? -1 : 0;
+      b /= 2;
+      ++ i;
+      ++ ntrials;
+
+      if (ntrials==0) {
+        oput_cstr_OFile (stdout_OFile (), "Overflow\n");
+        flush_OFile (stdout_OFile ());
+      }
+    }
+  }
+
+  oput_ujint_OFile (stdout_OFile (), ntrials);
+  oput_char_OFile (stdout_OFile (), '\n');
+  lose_sysCx ();
+  return 0;
+}
+
---
+++ pazucarlo.cc
@@ -0,0 +1,158 @@
+
+extern "C" {
+#include "cx/syscx.h"
+#include "cx/alphatab.h"
+}
+#include "cx/ofile.hh"
+#include "cx/table.hh"
+#include "cx/urandom.hh"
+
+bool has_match(const Cx::Table<uint>& cells, uint ncols)
+{
+  const uint nrows = cells.sz() / ncols;
+  for (uint r0 = 0; r0 < nrows; ++r0) {
+    uint r1 = r0+1;
+    uint r2 = r0+2;
+    for (uint c0 = 0; c0 < ncols; ++c0) {
+      uint c1 = c0+1;
+      uint c2 = c0+2;
+
+      bool hmatch = false;
+      bool vmatch = false;
+      if (c2 < ncols) {
+        hmatch =
+          (cells[r0*ncols+c0] == cells[r0*ncols+c1] && 
+           cells[r0*ncols+c0] == cells[r0*ncols+c2]);
+      }
+      if (r2 < nrows) {
+        vmatch =
+          (cells[r0*ncols+c0] == cells[r1*ncols+c0] && 
+           cells[r0*ncols+c0] == cells[r2*ncols+c0]);
+      }
+
+      if (hmatch || vmatch)
+        return true;
+    }
+  }
+  return false;
+}
+
+int main(int argc, char** argv)
+{
+  int argi =
+    (init_sysCx (&argc, &argv),
+     1);
+  Cx::OFile olog( stdout_OFile() );
+  bool matches_allowed = false;
+  uint ntotalorbs = 0;
+  uint ntotalcolors = 0;
+  uint ncolors = 0;
+  uint nmatches = 0;
+  uint npermatch = 0;
+  uint ntrials = 0;
+
+  if (argv[argi] && xget_uint_cstr (&nmatches, argv[argi]))
+    argi += 1;
+  else
+    nmatches = 4;
+
+  if (argv[argi] && xget_uint_cstr (&ncolors, argv[argi]))
+    argi += 1;
+  else
+    ncolors = 4;
+
+  if (argv[argi] && xget_uint_cstr (&npermatch, argv[argi]))
+    argi += 1;
+  else
+    npermatch = 3;
+
+  if (argv[argi] && xget_uint_cstr (&ntotalcolors, argv[argi]))
+    argi += 1;
+  else
+    ntotalcolors = 6;
+
+  if (argv[argi] && xget_uint_cstr (&ntotalorbs, argv[argi])) {
+    matches_allowed = true;
+    argi += 1;
+  }
+  else
+    ntotalorbs = 30;
+
+  if (argv[argi] && xget_uint_cstr (&ntrials, argv[argi]))
+    argi += 1;
+  else
+    ntrials = 1000000;
+
+  olog
+    << "\nMatches: " << nmatches
+    << "\nColors: " << ncolors
+    << "\nOrbs per match: " << npermatch
+    << "\nTotal colors: " << ntotalcolors
+    << "\nTotal orbs: " << ntotalorbs
+    << "\nTrials: " << ntrials
+    << "\nMatches allowed on board? " << (matches_allowed ? "yes" : "no")
+    << olog.endl();
+
+  if (ntotalcolors < ncolors) {
+    failout_sysCx("Not enough total colors.");
+    assert(0);
+  }
+  if (ncolors < nmatches) {
+    failout_sysCx("Not enough colors to match.");
+    assert(0);
+  }
+  if (ntrials == 0) {
+    failout_sysCx("Not enough trials.");
+    assert(0);
+  }
+
+  Cx::URandom urandom;
+  urandom.use_system_urandom(true);
+
+  Cx::Table<uint> counts( ntotalcolors );
+  uint npasses = 0;
+  uint nvalids = 0;
+
+  Cx::Table<uint> cells( ntotalorbs );
+  for (uint trial_idx = 0; trial_idx < ntrials; ++trial_idx) {
+    for (uint i = 0; i < cells.sz(); ++i) {
+      cells[i] = urandom.pick(ntotalcolors);
+    }
+
+    for (uint i = 0; i < counts.sz(); ++i) {
+      counts[i] = 0;
+    }
+    for (uint i = 0; i < cells.sz(); ++i) {
+      counts[cells[i]] += 1;
+    }
+
+    if (!matches_allowed) {
+      if (has_match (cells, 6))
+        continue;
+    }
+    nvalids += 1;
+
+    uint nmatched = 0;
+    for (uint i = 0; i < ncolors; ++i) {
+      if (counts[i] >= npermatch)
+        nmatched += 1;
+    }
+    if (nmatched >= nmatches) {
+      npasses += 1;
+    }
+  }
+
+  if (nvalids > 0)
+  {
+    double p = 100 * ((double) npasses / nvalids);
+    fprintf(stdout, "\nPercentage: %f\n", p);
+    fprintf(stdout, "\nValid boards: %f\n", (double)nvalids / ntrials);
+  }
+  else {
+    fprintf(stdout, "\nPercentage: N/A\n");
+  }
+
+  lose_sysCx ();
+  return 0;
+}
+
